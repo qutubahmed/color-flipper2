3dbf9fc5c0fd506246525eeee5d4b9d35c99342e
qutub ahmed <qutubahmed104@gmail.com>
2024-05-20 15:09:40 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+function themechanger() {
+    if (document.body.style.backgroundColor == 'black') {
+        document.body.style.backgroundColor = 'white'
+        document.body.style.color = 'black'
+        document.querySelector('.color').innerHTML = 'white'
+        document.getElementById("myDiv").style.borderColor = "black";
+    }
+    else {
+        document.body.style.color = 'white'
+        document.body.style.backgroundColor = 'black'
+        document.querySelector('.color').innerHTML = 'black'
+        document.getElementById("myDiv").style.borderColor = "white";
+    }
+
+}
